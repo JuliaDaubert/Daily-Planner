103f1dca1eb56a48dfe369b4b7224c58b500e424
Julia Daubert <daubertjulia@yahoo.com>
2020-09-25 15:06:19 +0800

    Time and Date added

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log ("Lets go")
+//Variables
+var displayDate  = document.getElementById ("currentDay");
+
+//Date: Weekday, short month, Date, short Year
+//var currentDate = moment().format('dddd') + moment().format("MMM Do YY");
+//console.log (currentDate);
+//Time: 
+//var currentTime = moment().format("hh:mm:ss A");
+//console.log(currentTime);
+
+
+
+
+// Date and hour 
+
+// Display Date - Get time (no display)
+function timeUpdate() {
+        var today = moment ();
+        var currentDate = today.format('dddd') + today.format(", Do MMM YY");
+        //console.log (currentDate)
+        var currentTime = today.format("hh:mm:ss A");
+        //console.log(currentTime);
+        displayDate.textContent = currentDate;
+};
+
+//Set in intervall to be updated every sec
+setInterval (timeUpdate, 1000);
+//console.log(setInterval);
+timeUpdate ();
+//console.log(timeUpdate);
+
+
+//Change color style according to current time
+
+
+
+
+
+
+
+//Store UserInput and Time in local storage 
+
